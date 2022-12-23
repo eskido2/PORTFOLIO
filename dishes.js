35c049e8f9a2b544f3c5654440b3589582912e1f
eskido2 <119795552+eskido2@users.noreply.github.com>
2022-12-23 05:53:22 -0800

    Add files via upload

--- dishes.js
+++ dishes.js
@@ -1,187 +1,21 @@
-export const DISHES =
-    [
-        {
-            id: 0,
-            name: 'Uthappizza',
-            image: 'assets/images/uthappizza.png',
-            category: 'mains',
-            label: 'Hot',
-            price: '4.99',
-            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-            comments: [
-                {
-                    id: 0,
-                    rating: 5,
-                    comment: "Imagine all the eatables, living in conFusion!",
-                    author: "John Lemon",
-                    date: "2012-10-16T17:57:28.556094Z"
-                },
-                {
-                    id: 1,
-                    rating: 4,
-                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                    author: "Paul McVites",
-                    date: "2014-09-05T17:57:28.556094Z"
-                },
-                {
-                    id: 2,
-                    rating: 3,
-                    comment: "Eat it, just eat it!",
-                    author: "Michael Jaikishan",
-                    date: "2015-02-13T17:57:28.556094Z"
-                },
-                {
-                    id: 3,
-                    rating: 4,
-                    comment: "Ultimate, Reaching for the stars!",
-                    author: "Ringo Starry",
-                    date: "2013-12-02T17:57:28.556094Z"
-                },
-                {
-                    id: 4,
-                    rating: 2,
-                    comment: "It's your birthday, we're gonna party!",
-                    author: "25 Cent",
-                    date: "2011-12-02T17:57:28.556094Z"
-                }
-            ]
-        },
-        {
-            id: 1,
-            name: 'Zucchipakoda',
-            image: 'assets/images/zucchipakoda.png',
-            category: 'appetizer',
-            label: '',
-            price: '1.99',
-            description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-            comments: [
-                {
-                    id: 0,
-                    rating: 5,
-                    comment: "Imagine all the eatables, living in conFusion!",
-                    author: "John Lemon",
-                    date: "2012-10-16T17:57:28.556094Z"
-                },
-                {
-                    id: 1,
-                    rating: 4,
-                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                    author: "Paul McVites",
-                    date: "2014-09-05T17:57:28.556094Z"
-                },
-                {
-                    id: 2,
-                    rating: 3,
-                    comment: "Eat it, just eat it!",
-                    author: "Michael Jaikishan",
-                    date: "2015-02-13T17:57:28.556094Z"
-                },
-                {
-                    id: 3,
-                    rating: 4,
-                    comment: "Ultimate, Reaching for the stars!",
-                    author: "Ringo Starry",
-                    date: "2013-12-02T17:57:28.556094Z"
-                },
-                {
-                    id: 4,
-                    rating: 2,
-                    comment: "It's your birthday, we're gonna party!",
-                    author: "25 Cent",
-                    date: "2011-12-02T17:57:28.556094Z"
-                }
-            ]
-        },
-        {
-            id: 2,
-            name: 'Vadonut',
-            image: 'assets/images/vadonut.png',
-            category: 'appetizer',
-            label: 'New',
-            price: '1.99',
-            description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
-            comments: [
-                {
-                    id: 0,
-                    rating: 5,
-                    comment: "Imagine all the eatables, living in conFusion!",
-                    author: "John Lemon",
-                    date: "2012-10-16T17:57:28.556094Z"
-                },
-                {
-                    id: 1,
-                    rating: 4,
-                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                    author: "Paul McVites",
-                    date: "2014-09-05T17:57:28.556094Z"
-                },
-                {
-                    id: 2,
-                    rating: 3,
-                    comment: "Eat it, just eat it!",
-                    author: "Michael Jaikishan",
-                    date: "2015-02-13T17:57:28.556094Z"
-                },
-                {
-                    id: 3,
-                    rating: 4,
-                    comment: "Ultimate, Reaching for the stars!",
-                    author: "Ringo Starry",
-                    date: "2013-12-02T17:57:28.556094Z"
-                },
-                {
-                    id: 4,
-                    rating: 2,
-                    comment: "It's your birthday, we're gonna party!",
-                    author: "25 Cent",
-                    date: "2011-12-02T17:57:28.556094Z"
-                }
-            ]
-        },
-        {
-            id: 3,
-            name: 'ElaiCheese Cake',
-            image: 'assets/images/elaicheesecake.png',
-            category: 'dessert',
-            label: '',
-            price: '2.99',
-            description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-            comments: [
-                {
-                    id: 0,
-                    rating: 5,
-                    comment: "Imagine all the eatables, living in conFusion!",
-                    author: "John Lemon",
-                    date: "2012-10-16T17:57:28.556094Z"
-                },
-                {
-                    id: 1,
-                    rating: 4,
-                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                    author: "Paul McVites",
-                    date: "2014-09-05T17:57:28.556094Z"
-                },
-                {
-                    id: 2,
-                    rating: 3,
-                    comment: "Eat it, just eat it!",
-                    author: "Michael Jaikishan",
-                    date: "2015-02-13T17:57:28.556094Z"
-                },
-                {
-                    id: 3,
-                    rating: 4,
-                    comment: "Ultimate, Reaching for the stars!",
-                    author: "Ringo Starry",
-                    date: "2013-12-02T17:57:28.556094Z"
-                },
-                {
-                    id: 4,
-                    rating: 2,
-                    comment: "It's your birthday, we're gonna party!",
-                    author: "25 Cent",
-                    date: "2011-12-02T17:57:28.556094Z"
-                }
-            ]
-        }
-    ];
+import * as ActionTypes from './ActionTypes';
+
+export const Dishes = (state = {
+      isLoading: true,
+      errMess: null,
+      dishes: []
+   }, action) => {
+   switch(action.type) {
+      case ActionTypes.ADD_DISHES:
+         return {...state, isLoading: false, errMess: null, dishes: action.payload}; 
+              
+      case ActionTypes.DISHES_LOADING:
+         return {...state, isLoading: true, errMess: null, dishes: []}; 
+         
+      case ActionTypes.DISHES_FAILED:
+         return {...state, isLoading: false, errMess: action.payload, dishes: []}; 
+        
+      default:
+         return state;
+   }
+}
